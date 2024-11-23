89208e635708585fd6582523bb1b1ab4cbea8349
EXLF <2760869618@qq.com>
2024-11-23 19:28:23 +0800

    只保留一个ipfs节点

--- getTokenInfo.js
+++ getTokenInfo.js
@@ -131,8 +131,8 @@ async function fetchTokenData() {
 
 // 获取元数据
 const IPFS_GATEWAYS = [
-    'https://gateway.pinata.cloud/ipfs/',  // Pinata 网关优先
-    // 'https://ipfs.io/ipfs/'                // IPFS.io 作为备用
+    // 'https://gateway.pinata.cloud/ipfs/',  // Pinata 网关优先
+    'https://ipfs.io/ipfs/'              // IPFS.io 作为备用
 ];
 
 async function fetchMetadata(url) {
